80d45eb2e476d3b95c22ed535fbe13474ead403e
Thomas Steinbach <thomas.steinbach@aikq.de>
2016-07-09 00:05:51 +0200

    smaller bugfix in start script

--- files/system/start.sh
+++ files/system/start.sh
@@ -7,8 +7,6 @@ docker run -it --rm \
   -v "$HOME/dwarf-fortress/movies:/df_linux/data/movies" \
   -e DISPLAY=:0.0 \
   -v /tmp/.X11-unix:/tmp/.X11-unix \
-# this is for sound, currently turned off
-#  --device=/dev/snd:/dev/snd \
   thomass/dwarf-fortress /df_linux/df
 
 xhost -local:dwarf-fortress
